fd53a3a652bb240536d02bdd042230879e7476ed
olesiaMartushkanova <olesia.martushkanova@seeeverything.com>
2021-08-30 20:58:10 +1200

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import CheerUpButton from './components/CheerUpButton/CheerUpButton';
+import Title from './components/Title/Title';
+
 const App = () => {
-  return <div className='App'>Do you feel sad today?</div>;
+  const onClickHandler = () => {
+    console.log('On click...');
+  };
+
+  return (
+    <div>
+      <Title className='title' text='Do you feel sad today?' />
+      <CheerUpButton
+        className='button__cheer-up'
+        text='Yes, cheer me up'
+        onClick={onClickHandler}
+      />
+    </div>
+  );
 };
 
 export default App;
